2910592fd4bb1a8af0609961f22b1b4029ba2c83
chunderella <sijy19@naver.com>
2022-07-07 20:24:40 +0900

    arr

---
+++ rabbit-project/src/main/java/com/rabbithole/lang/practice000.java
@@ -0,0 +1,13 @@
+public class practice000 {
+
+    public static void main(String[] args) {
+
+        int[] score = new int[4];
+
+        score[3] = 100;
+    
+        System.out.println("scroe[3]");
+    
+    }
+}
+
---
+++ rabbit-project/src/main/java/com/rabbithole/lang/practice0000.java
@@ -0,0 +1,16 @@
+public class practice0000 {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[4];
+      
+        for(int i=0;i<5;i++) {
+            System.out.println(arr[i]);
+    
+        }
+
+
+        
+    }
+}
+
